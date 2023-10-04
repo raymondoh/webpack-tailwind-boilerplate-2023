6d7195ac417295d09e770f2df2f76b969499e35e
Raymondo <raymondmhylton@gmail.com>
2023-10-04 11:08:58 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   //content: ["./**/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#0fa9e6",
+          light: "#3fbaeb",
+          dark: "#0c87b8",
+        },
+      },
+      fontFamily: {
+        headline: ["Poppins, sans-serif"], // font-headline // for text color only
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
